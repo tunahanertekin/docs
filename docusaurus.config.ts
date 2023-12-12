44d09efa70a83ca4775d2e2223a437665a69cb4b
tunahanertekin <tunahan.ertekin13@gmail.com>
2023-12-12 10:46:44 +0300

    chore(config): update base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,10 +8,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://robolaunch.io",
+  url: "https://robolaunch.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/ui-docs/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
